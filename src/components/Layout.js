e48bff25ed3848fe35fb18ce14502e0dc18a2112
sojinchoi <dvega.lyla@gmail.com>
2025-01-05 20:23:04 +0900

    feat: 레이아웃 설정 #1

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import { Container } from "./StyledComponents";
+import { Outlet } from "react-router-dom"; // 자식 라우트 컴포넌트 렌더링
+import Header from "./header/Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Outlet />
+      </Container>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/StyledComponents.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 4rem 2rem;
+`;
+
+export { Container };
