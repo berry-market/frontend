ada660b564ed63054c7464c62cc6022c4fb6f309
sojinchoi <dvega.lyla@gmail.com>
2025-01-07 22:21:16 +0900

    feat: 로그인, 로그아웃 Api 설정 #2

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -1,3 +1,3 @@
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-export const API_URL = `${SERVER_URL}/api/v1`;
-export const ADMIN_URL = `${SERVER_URL}/admin/v1`;
+export const API_URL = `/api/v1`;
+export const ADMIN_URL = `/admin/v1`;
---
+++ src/api/AuthApi.js
@@ -0,0 +1,14 @@
+import api from "../utils/AxiosInterceptor";
+import { API_URL } from "./ApiConfig";
+
+export const login = async (data) => {
+  const response = await api.post(`${API_URL}/auth/login`, data, {
+    withCredentials: true,
+  });
+  return response;
+};
+
+export const logout = async () => {
+  const response = await api.post(`${API_URL}/auth/logout`);
+  return response;
+};
