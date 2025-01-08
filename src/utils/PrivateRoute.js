497b3c510a89465187746a24209c08b51b84fa9a
sojinchoi <dvega.lyla@gmail.com>
2025-01-08 14:42:11 +0900

    fix: UserContext에 user 없으면 로그인 페이지로 이동 추가 #2

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,16 +1,19 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useUser } from "./UserContext";
 
 const PrivateRoute = () => {
   const location = useLocation();
   const token = localStorage.getItem("Authorization");
+  const { user } = useUser();
 
-  // 인증되지 않은 경우 로그인 페이지로 이동
-  if (!token) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+  // 인증 상태 확인
+  const isAuthenticated = token && user;
 
-  // 인증된 경우 자식 컴포넌트 렌더링
-  return <Outlet />;
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 };
 
 export default PrivateRoute;
