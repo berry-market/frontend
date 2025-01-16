ac58a669d34f5c9ff1823845c8abac958c2bbea9
sojinchoi <dvega.lyla@gmail.com>
2025-01-16 10:34:28 +0900

    feat: app.js 결제, 마이페이지 프로필 부분 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./components/StyledComponents.js";
-import Layout from "./components/Layout.js";
+import Layout from "./components/layout/Layout.js";
 import { UserProvider } from "./utils/UserContext";
 import PrivateRoute from "./utils/PrivateRoute";
 import Login from "./pages/login/Login";
+import MypageLayout from "./components/layout/MypageLayout.js";
+import Profile from "./pages/mypage/profile/Profile.js";
+import CheckoutPage from "./pages/mypage/payments/CheckoutPage.js";
+import SuccessPage from "./pages/mypage/payments/response/SuccessPage.js";
+import FailPage from "./pages/mypage/payments/response/FailPage.js";
+import PaymentLayout from "./components/layout/PaymentLayout.js";
 
 function App() {
   return (
@@ -14,9 +20,20 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route path="/login" element={<Login />} />
-
-              {/* 인증이 필요한 페이지 */}
-              <Route element={<PrivateRoute />}></Route>
+              {/* 마이페이지 */}
+              <Route element={<PrivateRoute />}>
+                <Route path="/mypages" element={<MypageLayout />}>
+                  <Route path="profile" element={<Profile />} />
+                </Route>
+              </Route>
+            </Route>
+            {/* 결제페이지 */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/payments" element={<PaymentLayout />}>
+                <Route path="checkout" element={<CheckoutPage />} />
+                <Route path="success" element={<SuccessPage />} />
+                <Route path="fail" element={<FailPage />} />
+              </Route>
             </Route>
           </Routes>
         </div>
